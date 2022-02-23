66b980a604c4140257b8a9b52ebf541b6a5d4a91
chandani nonghanvadra <91254443+NonghanvadraChandani@users.noreply.github.com>
2022-02-23 13:08:24 +0530

    Create crud.py

---
+++ crud.py
@@ -0,0 +1,37 @@
+#import sqlite3
+import sqlite3
+
+#create database
+con=sqlite3.connect('crud.db')
+print('database created')
+
+#create table movie 
+con.execute('create table movie (leadactor text,actress text,year int,director text,moviename text)')
+print('table created')
+
+#insert record
+con.execute('insert into movie values("aurjun","samantha",2021,"rohit","hero")')
+con.execute('insert into movie values("aurjun kapur","nathasa",2011,"manish patel","india")')
+con.execute('insert into movie values("salaman khan","priynaka chopada",2019,"rohit","superhero")')
+
+con.commit()
+print('record inserted')
+
+#display record
+data=con.execute('select * from movie')
+for row in data:
+    print("Lead Actor:",row[0])
+    print("Actress:",row[1])
+    print("Year",row[2])
+    print("Director Name:",row[3])
+    print("Movie Name:",row[4])
+
+#select movie based on actross name
+print("====================")
+data=con.execute('select * from movie where actress="samantha"')
+for row in data:
+    print("Lead Actor:",row[0])
+    print("Actress:",row[1])
+    print("Year",row[2])
+    print("Director Name:",row[3])
+    print("Movie Name:",row[4])
